7e9ac6162947e0729d505b19a91b36674abb8839
Richard Wolf <32384198+richwolf817@users.noreply.github.com>
2021-01-20 12:26:33 -1000

    Update restart.py

--- restart.py
+++ restart.py
@@ -3,8 +3,9 @@ client = docker.from_env()
 
 for container in client.containers.list(): 
     image = container.attrs['Config']['Image']
-    if 'topograph-crawler' in image: 
+    if 'topograph-cicd' in image: 
         logs = container.logs(until=1800)
-        if not logs:
-            print(image)
+        if logs:
             container.restart()
+
+print("happy time")
