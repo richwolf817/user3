5df4957f24e508c39b4a907e5b6db778b703aa79
Richard Wolf <32384198+richwolf817@users.noreply.github.com>
2021-01-20 15:19:16 -1000

    Update restart.py

--- restart.py
+++ restart.py
@@ -1,12 +1,11 @@
-import docker 
+import docker
+import test
 client = docker.from_env() 
 
-print("what")
-for container in client.containers.list(): 
-    image = container.attrs['Config']['Image']
-    if 'topograph-cicd' in image: 
-        logs = container.logs(until=1800)
-        if logs:
-            container.restart()
+print("cha cha cha")
+if 'topograph-cicd' in image: 
+    logs = container.logs(until=1800)
+    if logs:
+        container.restart()
 
 print("happy time")
